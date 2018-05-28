488d4bf56d3226faf7faefffb42b29f79c8d12f7
Joe.zhou <yiyuzhou@greatwall.com.cn>
2018-05-28 14:23:24 +0800

    lombok整合

--- src/main/java/com/example/springbootmybatisdemo/entity/User.java
+++ src/main/java/com/example/springbootmybatisdemo/entity/User.java
@@ -1,11 +1,15 @@
 package com.example.springbootmybatisdemo.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author by yyz
  * on 2018/5/28
  */
 
+@Getter
+@Setter
 public class User {
 
     private Integer id;
@@ -16,35 +20,4 @@ public class User {
 
     private Integer age;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
