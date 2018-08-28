b6cea8c084bbaf3047f8fdbf09ab4be725498fc1
Joe.zhou <yiyuzhou@greatwall.com.cn>
2018-08-28 15:23:32 +0800

    代码更新

--- src/test/java/com/example/springbootmybatisdemo/UserControllerTest.java
+++ src/test/java/com/example/springbootmybatisdemo/UserControllerTest.java
@@ -2,6 +2,8 @@ package com.example.springbootmybatisdemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,11 +22,17 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 public class UserControllerTest {
 
+    /**
+     * 引入日志，注意都是"org.slf4j"包下
+     */
+    private final static Logger logger = LoggerFactory.getLogger(UserControllerTest.class);
+
     @Autowired
     private MockMvc mvc;
 
     @Test
     public void getUserList() throws Exception {
+        logger.info("进入日志测试logback");
         mvc.perform(MockMvcRequestBuilders.get("/user/getUserInfo"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
         //.andExpect(MockMvcResultMatchers.content().string("365"));  //测试接口返回内容
